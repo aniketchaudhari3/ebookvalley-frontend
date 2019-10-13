2d8b807e1f5536c5addbe7cfee0c942fc44f981e
Aniket Chaudhari <anikethc3@gmail.com>
2019-10-13 22:45:43 +0530

    added fetch

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Searchbar extends Component {
+    constructor(props){
+        super(props);
+        this.searchBook = this.searchBook.bind(this);
+    }
+
+    searchBook(value){
+        console.log(value);
+    }
+    componentDidMount() {
+        fetch("http://localhost:5000/api?q=crushing it", {mode:'cors'})
+          .then(res => console.log(res.json()))
+        //   .then(
+        //     (result) => {
+        //       this.setState({
+        //         isLoaded: true,
+        //         items: result.items
+        //       });
+        //     },
+            
+        //     (error) => {
+        //       this.setState({
+        //         isLoaded: true,
+        //         error
+        //       });
+        //     }
+        //   )
+      }
 
     render() {
         return (
 
             <div class="col-md-12 input-group mb-3">
                 
-                <input type="text" placeholder="Search Ebooks" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                <input id="searchbar" onChange={() => {this.searchBook(document.getElementById('searchbar').value)}} type="text" placeholder="Search Ebooks" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
                 <div class="input-group-append">
                     <span class="input-group-text" id="inputGroup-sizing-default">Search</span>
                 </div>
